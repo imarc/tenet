4a7ae80729cf0d158ff0b3bfdbbd2b1cfbc67d2c
Jeff Turcotte <jeff.turcotte@gmail.com>
2014-03-14 12:00:09 -0400

    Fixed issue with associating an related instance, and some refactoring

--- src/Filter/AbstractAssociationFilter.php
+++ src/Filter/AbstractAssociationFilter.php
@@ -9,48 +9,38 @@ abstract class AbstractAssociationFilter
 	 */
 	protected function makeObject($accessor, $object, $field, $data)
 	{
-		/*
-		$manager     = $accessor->getObjectManager();
-		$metadata    = $manager->getClassMetadata($target);
-		$identifiers = $metadata->getIdentifierFieldNames();
-		$target      = $metadata->getAssociationTargetClass($field);
-		 */
-
 		$manager        = $accessor->getObjectManager();
 		$objectMetadata = $manager->getClassMetadata(get_class($object));
 		$target         = $objectMetadata->getAssociationTargetClass($field);
 		$targetMetadata = $manager->getClassMetadata($target);
 		$identifiers    = $targetMetadata->getIdentifierFieldNames();
 
-		// handle object of type target
 		if ($data instanceof $target) {
-			return $data;
-		}
+			// handle object of type target
+			$targetObject = $data;	
+		} else if (is_scalar($data) && count($identifiers) === 1) {
+			// handle scalar identifier
+			$targetObject = $manager->find($target, $data) ?: new $target;
+			$accessor->set($targetObject, $identifiers[0], $data);
+		} else {
+			// handle compound identifiers
+			$targetObject = new $target();
 
-		// handle scalar identifier
-		if (is_scalar($data) && count($identifiers) === 1) {
-			return $manager->find($target, $data) ?: new $target;
-		}
+			if (is_array($data)) {
+				// get array of identifier data passed in
+				$ids = array_filter(array_intersect_key($data, array_flip($identifiers)), function($v) {
+					return ($v === '' || $v === null) ? false : true;
+				});
 
-		$targetObject = new $target;
-
-		// handle keyed identifier(s)
-		if (is_array($data)) {
-			// get array of identifier data passed in
-			$ids = array_filter(array_intersect_key($data, array_flip($identifiers)), function($v) {
-				return ($v === '' || $v === null) ? false : true;
-			});
-
-			// if all identifiers are present, try to find the object
-			if (count($ids) === count($identifiers)) {
-				$existing_record = $manager->find($target, $ids);
-
-				if ($existing_record) {
-					$targetObject = $existing_record;
+				// if all identifiers are present, try to find the object
+				if (count($ids) === count($identifiers)) {
+					if ($existing_record = $manager->find($target, $ids)) {
+						$targetObject = $existing_record;
+					}
 				}
-			}
 
-			$accessor->fill($targetObject, $data);
+				$accessor->fill($targetObject, $data);
+			}
 		}
 
 		if ($objectMetadata->isAssociationInverseSide($field)) {
